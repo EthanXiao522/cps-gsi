75bca07cff9307106579b19b5199477e4ccf421a
EthanXiaoG <ethanxiao522@gmail.com>
2026-05-02 22:45:57 +0800

    添加项目cps-be-go,生成合约对应的go文件

---
+++ cps-be-go/utils/Convert.go
@@ -0,0 +1,71 @@
+// convert.go
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type ForgeArtifact struct {
+	ABI      json.RawMessage `json:"abi"`
+	Bytecode struct {
+		Object string `json:"object"`
+	} `json:"bytecode"`
+}
+
+type CombinedJSON struct {
+	Contracts map[string]struct {
+		ABI json.RawMessage `json:"abi"`
+		Bin string          `json:"bin"`
+	} `json:"contracts"`
+}
+
+/**
+ * 这个工具将 Foundry 的 artifact 转换为 combined-json 格式，方便 Go 代码使用。
+ * 运行前请确保已经编译了合约，并且路径正确。
+ * 输出文件将保存在 ../contracts/ 目录下，命名为 <ContractName>-combined.json。
+ * 用法: `go run utils/Convert.go MyToken` 或在 utils 目录下 `go run convert.go MyToken`
+ */
+func main() {
+	// contractName 从命令行第一个参数获取：
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "缺少参数,命令行用法: go run Convert.go <ContractName>")
+		os.Exit(2)
+	}
+	contractName := os.Args[1]
+	data, err := os.ReadFile("../../cps-contracts/out/" + contractName + ".sol/" + contractName + ".json")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failed to read artifact:", err)
+		os.Exit(1)
+	}
+	var artifact ForgeArtifact
+	if err := json.Unmarshal(data, &artifact); err != nil {
+		fmt.Fprintln(os.Stderr, "failed to unmarshal artifact:", err)
+		os.Exit(1)
+	}
+
+	// 转换为 combined-json 格式
+	combined := CombinedJSON{
+		Contracts: map[string]struct {
+			ABI json.RawMessage `json:"abi"`
+			Bin string          `json:"bin"`
+		}{
+			contractName + ".sol:" + contractName: {
+				ABI: artifact.ABI,
+				Bin: artifact.Bytecode.Object,
+			},
+		},
+	}
+
+	output, err := json.MarshalIndent(combined, "", "  ")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failed to marshal combined json:", err)
+		os.Exit(1)
+	}
+	if err := os.WriteFile("../contracts/"+contractName+"-combined.json", output, 0644); err != nil {
+		fmt.Fprintln(os.Stderr, "failed to write output file:", err)
+		os.Exit(1)
+	}
+	fmt.Println("wrote sucess!! path: ../contracts/" + contractName + "-combined.json")
+}
